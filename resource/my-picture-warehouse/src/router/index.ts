d1c72a50c45c7f2bb3c1122f796dd7388f081eb8
YT.NIE <13920365339@163.com>
2022-05-08 03:22:15 +0800

    feat：添加文件仓库项目

---
+++ resource/my-picture-warehouse/src/router/index.ts
@@ -0,0 +1,57 @@
+// Copyright 2021-2022 the xiejiahe. All rights reserved. MIT license.
+
+import type { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import NewFile from '@/views/NewFile.vue'
+import Mobile from '@/views/Mobile.vue'
+import { isMobile } from '@/utils'
+import { getLocalIsLogin } from '@/utils/storage'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: isMobile() ? Mobile : Home,
+  },
+  {
+    path: '/app',
+    name: 'Mobile',
+    component: Mobile,
+  },
+  {
+    path: '/login',
+    alias: ['/oauth/redirect'],
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/file/new',
+    name: 'NewFile',
+    component: NewFile,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NoMatch',
+    redirect: '/',
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+router.beforeEach(to => {
+  const isLogin = getLocalIsLogin()
+  if (to.name === 'Login' && isLogin) {
+    router.replace('/')
+  } else {
+    if (!isLogin && to.name !== 'Login') {
+      router.replace('/login')
+    }
+  }
+})
+
+export default router
